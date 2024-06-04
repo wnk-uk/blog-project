94dc59adc0d45c59a721558af1e8051fe6840a0d
wnk-uk <dailycoding0211@gmail.com>
2024-06-04 20:24:08 +0900

    vue3.js add

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  import AppHeader from '.';
+
+  data() {
+    return {
+      key: value
+    }
+  },
+  components: {
+    AppHeader,
+    AppNav
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
